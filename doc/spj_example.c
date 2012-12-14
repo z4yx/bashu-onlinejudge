b49ee6bd14e3630ad74041ca9d11a2dad7d9a695
Zhang <593141477@qq.com>
2012-12-14 21:14:57 +0800

    fix bug in special judge example

--- doc/spj_example.c
+++ doc/spj_example.c
@@ -11,15 +11,15 @@ int main(int argc,char *argv[])
 	fstd=fopen(argv[2],"r");//打开测试点标准输出文件
 	score=atoi(argv[1]);//取得测试点的满分
 
-	fin=fopen("data.in","r");//测试数据的输入
-	fout=fopen("data.out","w");//选手的输出
+	fin=fopen("user.in","r");//测试数据的输入
+	fout=fopen("user.out","w");//选手的输出
 
 	//评测
 
 	if (/*完全正确*/)
 	{
 		fprintf(fscore,"%d",score);//返回满分
-		fprintf(freport,"right");//报告Judge结果为right
+		fprintf(freport,"right");//报告用户结果,内容任意
 	}
 	else if (/*部分正确*/)
 	{
@@ -29,7 +29,7 @@ int main(int argc,char *argv[])
 	else
 	{
 		fprintf(fscore,"%d",0);//返回0分
-		fprintf(freport,"wrong");//报告Judge结果为wrong
+		fprintf(freport,"wrong");//报告用户结果,内容任意
 	}
 
 	fclose(fscore);//关闭得分文件
