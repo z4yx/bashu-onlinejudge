ac3f2ec8b4ec583d6ab1308a94903b56d7c512c1
Zhang <593141477@qq.com>
2013-01-19 22:00:49 +0800

    allow google bot

--- web/code/inc/checklogin.php
+++ web/code/inc/checklogin.php
@@ -2,8 +2,10 @@
 //NOT use global authorization
 define('REQUIRE_AUTH',0);
 
+define('DISALLOW_GOOGLEBOT',0);
+
 session_start();
-if(!isset($_SESSION['user'])){
+if(!isset($_SESSION['user']) && (DISALLOW_GOOGLEBOT || FALSE===strstr($_SERVER['HTTP_USER_AGENT'],'Googlebot'))){
   require_once 'inc/cookie.php';
   if(!check_cookie()) {
     if(REQUIRE_AUTH) {
