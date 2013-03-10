88bc85df326246f4c02f5829fc79d46aa83c6d90
Zhang <593141477@qq.com>
2013-03-10 10:18:41 +0800

    use webfonts in Mathjax

--- web/code/inc/mathjax_head.php
+++ web/code/inc/mathjax_head.php
@@ -1,14 +1,13 @@
 <script type="text/x-mathjax-config">
 MathJax.Hub.Config({
 	extensions: ["tex2jax.js"],
-	jax: ["input/TeX", "output/HTML-CSS"],
+	// jax: ["input/TeX", "output/HTML-CSS"],
 	tex2jax: {
 	  inlineMath: [ ['[inline]','[/inline]'] ],
-	  displayMath: [ ["[tex]","[/tex]"] ],
-	  processEscapes: true
+	  displayMath: [ ["[tex]","[/tex]"] ]
 	},
-	"HTML-CSS": { availableFonts: ["TeX"], imageFont: null }
+	"HTML-CSS": { availableFonts: ["TeX"], webFont: "TeX", imageFont: null }
 });
 </script>
 <!--<script type="text/javascript"  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>-->
-<script type="text/javascript" src="../assets/Mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+<script async type="text/javascript" src="../assets/Mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
