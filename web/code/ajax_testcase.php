c6e33f660936ccecc798ddc2e4d982c8d60c66dc
Zhang <593141477@qq.com>
2014-03-05 18:27:59 +0800

    filter uploaded file name

--- web/code/ajax_testcase.php
+++ web/code/ajax_testcase.php
@@ -29,6 +29,12 @@ function upload_file_handler($targetDir)
 	} else {
 		$fileName = uniqid("file_");
 	}
+	
+	$fileName = basename($fileName);
+	if($fileName=='' || $fileName=='.' || $fileName=='..' ){
+		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+		die('{"jsonrpc" : "2.0", "error" : {"code": 104, "message": "Illegal file name."}, "id" : "id"}');
+	}
 	$filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
 
 	// Chunking might be enabled
