3e02080785a9a08750c0b2f24193007e0f6d62ff
Zhang <593141477@qq.com>
2014-07-27 11:07:22 +0800

    merge edit.js to common.js

--- web/assets/js/common.js
+++ web/assets/js/common.js
@@ -26,6 +26,40 @@ function LoadCSS(url) {
    head.appendChild(link);
    return link;
  }
+function InsertString(tb, str){
+    //var tb = document.getElementById(tbid);
+    tb.focus();
+    if (document.all){
+        var r = document.selection.createRange();
+        document.selection.empty();
+        r.text = str;
+        r.collapse();
+        r.select();
+    }
+    else{
+        var newstart = tb.selectionStart+str.length;
+        tb.value=tb.value.substr(0,tb.selectionStart)+str+tb.value.substring(tb.selectionEnd);
+        tb.selectionStart = newstart;
+        tb.selectionEnd = newstart;
+    }
+}
+function GetSelection(tb){
+
+    var sel = '';
+    if (document.all){
+        var r = document.selection.createRange();
+        document.selection.empty();
+        sel = r.text;
+    }
+    else{
+    	//var tb = document.getElementById(tbid);
+    	// tb.focus();
+        var start = tb.selectionStart;
+        var end = tb.selectionEnd;
+        sel = tb.value.substring(start, end);
+    }
+    return sel;
+}
 function GetUrlParms()    
 {
     var args=new Object();
