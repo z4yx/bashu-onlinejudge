d68fbc9847ab97b25e2a45f3695e88ef3504f4d6
sqd <dragonazd@gmail.com>
2015-07-07 10:23:56 +0800

    Update about.php

--- web/code/about.php
+++ web/code/about.php
@@ -14,7 +14,7 @@ $Title="About Bashu OnlineJudge";
           <div class="page-header">
             <h2>About</h2>
           </div>
-          <p>Bashu OnlineJudge is a system designed for OI training.You can find problems in the problemset
+          <p>Bashu OnlineJudge is a system designed for OI training. You can find problems in the problemset
              and submit solutions. OnlineJudge system allows you to test your solution for every problem.</p>
           <p>Bashu OnlineJudge is an open source project.</p>
           <p>&nbsp;</p>
@@ -27,8 +27,8 @@ $Title="About Bashu OnlineJudge";
             <h2>FAQ</h2>
           </div>
           <div>
-            <h3>Q: What is the compiler the judge is using and what are the compiler options?</h3>
-            A: We are using GNU GCC/G++ for C/C++ compile, Free Pascal for pascal compile. The compile options are:
+            <h3>Q: What compilers does the judge use and what are the compiler options?</h3>
+            A: We are using GNU GCC/G++ for C/C++ compilation, and Free Pascal for pascal compilation. The compiler options are:
             <table class="table table-striped table-bordered table-condensed table-last-left-aligned" id="tab_options">
               <tbody>
                 <tr>
@@ -52,38 +52,38 @@ $Title="About Bashu OnlineJudge";
           </div>
           <hr>
           <div>
-            <h3>Q: Where are the input and the output?</h3>
-            A: Your program shall read input from stdin('Standard Input') and write output to stdout('Standard Output').For example,you can use 'scanf' in C or 'cin' in C++ to read from stdin,and use 'printf' in C or 'cout' in C++ to write to stdout.<br>User programs are not allowed to open and read from/write to files, you will get a "Runtime Error" if you try to do so.
+            <h3>Q: From/to where should the program read/write the input/output?</h3>
+            A: Your program shall read input from stdin('Standard Input') and write output to stdout('Standard Output'). To be specific, you can use 'scanf' in C and 'cin' in C++ to read from stdin, and use 'printf' in C and 'cout' in C++ to write to stdout.<br>User programs are not allowed to open and read from/write to files, "Runtime Error" would occure if your programs try to do so.
           </div>
           <hr>
           <div>
-            <h3>Q: What is the meaning of the judge's replies?</h3>
+            <h3>Q: What do the judge's replies mean?</h3>
             A: Here is a list of the judge's replies and their meaning:<br>
             <table class="table table-condensed table-last-left-aligned" id="about_msg">
               <tbody>
                 <tr>
                   <td><span class="label label-success">Accepted</span></td>
-                  <td>OK! Your program is correct!.</td>
+                  <td>OK! Your program is correct!</td>
                 </tr>
                 <tr>
                   <td><span class="label">Compile Error</span></td>
-                  <td>The compiler could not compile your program. Of course, warning messages are not error messages.</td>
+                  <td>The compiler could not compile your program. Of course, warnings during compilation wouldn't be regarded as errors.</td>
                 </tr>
                 <tr>
                   <td><span class="label label-warning">Memory Exceeded</span></td>
-                  <td>Your program tried to use more memory than the problem settings.</td>
+                  <td>Your program tried to use more memory than the limit indicated by the problem settings.</td>
                 </tr>
                 <tr>
                   <td><span class="label label-info">Runtime Error</span></td>
-                  <td>Your program failed during the execution (illegal file access, stack overflow, pointer reference out of range, floating point exception, divided by zero...).</td>
+                  <td>Your program failed during the execution (illegal file access, stack overflow, pointer reference out of range, floating point exception, divided by zero, etc).</td>
                 </tr>
                 <tr>
                   <td><span class="label label-warning">Time Out</span></td>
-                  <td>Your program tried to run during too much time.</td>
+                  <td>Your program failed to ouput an answer within the time limit of the problem.</td>
                 </tr>
                 <tr>
                   <td><span class="label label-important">Wrong Answer</span></td>
-                  <td>The solution has not produced the desired output.</td>
+                  <td>Your solution has not produced the desired output.</td>
                 </tr>
                 <tr>
                   <td><span class="label label-inverse">Validator Error</span></td>
